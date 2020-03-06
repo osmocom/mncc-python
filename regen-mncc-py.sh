7b33e5f6e2a0d0e0e5b6af872bb127503c3a3b6f
Harald Welte <laforge@osmocom.org>
2020-03-06 10:43:58 +0000

    Fix regen-mncc-py.sh script to work on Debian8 (jessie)
    As python-ctypeslib is long unmaintained, it is not packaged
    for anything more recent than Debian 8 (jessie).  Let's print
    that at start and check for availability of the reqquired
    executable programs (part of python-ctypeslib).
    
    Change-Id: Ied6333016e2eb357bda08499742f1ed8c56ba762

--- regen-mncc-py.sh
+++ regen-mncc-py.sh
@@ -1,5 +1,16 @@
 #/bin/sh
-echo on Debian, codegen/cparser.py must call gccxml.real instead of gccxml!
+echo This script requires that python-ctypeslib is installed on the system
+echo Unfortunately, this package is long unmaintained and only available on Debian8
+echo
+
+if [ ! -x `which h2xml` ]; then
+	echo No h2xml executable found - python-ctypeslib not installed?
+fi
+
+if [ ! -x `which xml2py` ]; then
+	echo No xml2py executable found - python-ctypeslib not installed?
+fi
+
 cp ./mncc.h /tmp/mncc.h
-h2xml.py ./mncc.h -c -o mncc.xml
-xml2py.py mncc.xml -k dest -v -o mncc.py
+h2xml ./mncc.h -c -o mncc.xml
+xml2py mncc.xml -k dest -v -o mncc.py
